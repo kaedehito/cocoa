f702dc07f6618904a757f4f974f7165a77e1f44a
小笠原 楓人 <fucho.021000@gmail.com>
2024-10-19 19:39:58 +0900

    structs.rs の更新

--- src/structs.rs
+++ src/structs.rs
@@ -1,8 +1,9 @@
 #[allow(non_camel_case_types)]
-/// 現在の状態を構成する構造体
-/// now_dir、hostname、user_nameで構成されている
-/// now_dirには現在のカレントディレクトリ、hostnameには使用しているPCのhostname。
-/// user_nameには使用しているユーザー名が入る
+/// シェルの現在の状態を表す構造体。
+/// 構成要素は`now_dir`（カレントディレクトリ）、`hostname`（ホスト名）、`user_name`（ユーザー名）。
+/// - `now_dir`: 現在のカレントディレクトリのパスを格納する。
+/// - `hostname`: 使用中のPCのホスト名を格納する。
+/// - `user_name`: 現在のユーザー名を格納する。
 pub struct dir {
     pub now_dir: String,
     pub hostname: String,
