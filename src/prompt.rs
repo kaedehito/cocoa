7f91e8b9bd187e05d8fd62c2cde43ca5dd1a9e8f
小笠原 楓人 <fucho.021000@gmail.com>
2024-10-19 19:38:26 +0900

    prompt.rs の更新

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,26 +1,27 @@
 use crate::structs;
 use colored::{self, Colorize};
 
-// dirを実装
+// dir構造体の実装
 impl structs::dir {
-    /// promptを返す。
-    ///```text
+    /// プロンプト文字列を生成して返す関数。
+    /// テンプレート形式は次の通り：
+    /// ```
     /// username@hostname now_dir$
-    ///```
-    /// がテンプレート。
-    /// usernameがkaedeで、hostnameがarchlinux。カレントディレクトリがuserのhomeだとすると
-    ///```text
+    /// ```
+    /// 例: usernameが`kaede`、hostnameが`archlinux`で、カレントディレクトリがホームディレクトリの場合：
+    /// ```
     /// kaede@archlinux ~$
-    ///```
-    /// のように表示される
+    /// ```
+    /// カラー表示を行うため、ユーザー名やホスト名、ディレクトリ名には異なる色を適用。
     pub fn display_ps1(&self) -> String {
+        // プロンプトのフォーマットを作成し、それぞれの要素に色を適用
         let format = format!(
             "{}{}{} {}{} ",
-            self.user_name.cyan(),
-            "@".green(),
-            self.hostname.magenta(),
-            self.now_dir.bright_red(),
-            "$".bright_white()
+            self.user_name.cyan(),       // ユーザー名をシアンにカラーリング
+            "@".green(),                 // @を緑色に
+            self.hostname.magenta(),      // ホスト名をマゼンタに
+            self.now_dir.bright_red(),    // カレントディレクトリを明るい赤色に
+            "$".bright_white()            // プロンプトの終端`$`を明るい白色に
         );
         format
     }
